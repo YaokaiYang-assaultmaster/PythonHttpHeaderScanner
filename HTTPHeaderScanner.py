fa7882fc20106b2cdfc207f68249f849e916b3e4
YaokaiYang-assaultmaster <yxy586@case.edu>
2017-04-28 21:11:01 -0400

    Add detailed comment for code

--- HTTPHeaderScanner.py
+++ HTTPHeaderScanner.py
@@ -1,16 +1,21 @@
 import urllib2
+import sys
+
 
 class HttpHeaderScanner:
 
     def __init__(self):
+        """Constructor of the class
+
+        """
         self.__headers = {}
 
     def connect_server(self, website):
         """Connect a server of which the header fields we want to check
-        
+
         Args:
-            website (String): The website whose server we want to check 
-        
+            website (String):
+                The website whose server we want to check
         """
         req = urllib2.Request(website)
 
@@ -31,33 +36,36 @@ class HttpHeaderScanner:
 
     def get_server_field(self):
         """Return the server fields in the HTTP header
-        
+
         Returns:
-            String: The value of the server field, if not exists, return "None".
+            server (String):
+                The value of the server field, if not exists, return "None".
         """
 
+        server = ''
         if len(self.__headers) == 0:
-            return 'None'
+            server = 'None'
         elif 'server' in self.__headers:
-            return ('server', self.__headers['server'])
+            server = self.__headers['server']
         else:
-            return 'None'
+            server = 'None'
+
+        return server
 
     def show_headers(self):
         """Print out all the fields of the http header
-        
+
         """
         if len(self.__headers) == 0:
             print('Connection Error')
         for ele in self.__headers:
             print(str(ele) + ': ' + str(self.__headers[ele]) + '\n')
 
-
     def get_headers(self):
         """Return a dictionary of headers fields in (fieldname, value) pair
-        
+
         Returns:
-            Dict: A dictionary of (field name, corresponding value) pairs
+            tmp (dict): A dictionary of (field name, corresponding value) pairs
         """
         tmp = dict()
         if len(self.__headers) == 0:
@@ -70,13 +78,15 @@ class HttpHeaderScanner:
 
     def get_header_count(self):
         """Return the total number of different fields in the header
-        
+
         Returns:
-            int: number of the header fields. If connection error occured, return -1
+            header_count (int):
+                number of the header fields.
+                If connection error occured, return -1
         """
+        header_count = 0
         if len(self.__headers) == 0:
-            return -1
-        return len(self.__headers)
-
-
-
+            header_count = -1
+        else:
+            header_count = len(self.__headers)
+        return header_count
